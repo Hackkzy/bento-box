0061420d4dbd22466e9770c12c1ef393e2a67d1d
Jay Patel <pjay9981@gmail.com>
2025-02-14 16:09:53 +0530

    Fix scale effect not working on Frontend

--- src/bento-card/save.js
+++ src/bento-card/save.js
@@ -23,10 +23,10 @@ export default function save( { attributes } ) {
 				className: `hover-${ hoverEffect }`,
 				style: {
 					'--bbx-card-rotation-degree': `${ rotationDegree }deg`,
-					'--bbx-card-scale-factor': scaleFactor,
-					'--bbx-card-shadow-intensity': shadowIntensity,
+					'--bbx-card-scale-factor': `${ scaleFactor }`,
+					'--bbx-card-shadow-intensity': `${ shadowIntensity }px`,
 					'--bbx-card-tilt-degree': `${ tiltDegree }deg`,
-					'--bbx-card-glow-intensity': glowIntensity,
+					'--bbx-card-glow-intensity': `${ glowIntensity }px`,
 					'--bbx-card-hover-shadow-color': hoverShadowColor,
 					'--bbx-card-hover-glow-color': hoverGlowColor,
 					'--bbx-card-grid-row': `span ${ rowSpan }`,
