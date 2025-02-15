9cedd4dc51631402344bbb6ca74616724f4f3fbf
Jay Patel <pjay9981@gmail.com>
2025-02-15 13:12:13 +0530

    build generated

--- build/bento-box/index.asset.php
+++ build/bento-box/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-i18n'), 'version' => '16f3c21469d3976ad685');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-i18n'), 'version' => '687b2b935899ab85ecb1');
--- build/bento-card/index.asset.php
+++ build/bento-card/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => 'f975c18599e089e53476');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => '7fac17ed7034dac83c56');
