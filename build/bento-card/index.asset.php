1b458b51d4986bf61be7b2fb9f0a37d4dcfd3b35
Jay Patel <pjay9981@gmail.com>
2025-02-23 14:33:40 +0530

    Build generated

--- build/bento-card/index.asset.php
+++ build/bento-card/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => '7fac17ed7034dac83c56');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => 'b79897d1863bc068be68');
