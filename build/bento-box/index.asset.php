6cf84474fc0021fd31ca13fe2ceb5446c52f952f
Jay Patel <pjay9981@gmail.com>
2025-02-23 14:15:50 +0530

    Build generated

--- build/bento-box/index.asset.php
+++ build/bento-box/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-i18n'), 'version' => '687b2b935899ab85ecb1');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-i18n'), 'version' => 'ecfe0d36f7144244f67a');
