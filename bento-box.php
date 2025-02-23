89d25808def7a2073195eeebf2eb9897c06a1e12
Jay Patel <pjay9981@gmail.com>
2025-02-23 14:08:59 +0530

    Changed Prefix & Minor Code correction

--- bento-box.php
+++ bento-box.php
@@ -21,73 +21,81 @@ if ( ! defined( 'ABSPATH' ) ) {
 /**
  * Defining Constants.
  *
- * @package    bbx-bento-box
+ * @package bbx-bento-box
  */
-if ( ! defined( 'BBX_VERSION' ) ) {
+if ( ! defined( 'BENTOBOX_VERSION' ) ) {
 	/**
 	 * The version of the plugin.
 	 */
-	define( 'BBX_VERSION', '1.0.0' );
+	define( 'BENTOBOX_VERSION', '1.0.0' );
 }
 
-if ( ! defined( 'BBX_PATH' ) ) {
+if ( ! defined( 'BENTOBOX_PATH' ) ) {
 	/**
 	 *  The server file system path to the plugin directory.
 	 */
-	define( 'BBX_PATH', plugin_dir_path( __FILE__ ) );
+	define( 'BENTOBOX_PATH', plugin_dir_path( __FILE__ ) );
 }
 
-if ( ! defined( 'BBX_URL' ) ) {
+if ( ! defined( 'BENTOBOX_URL' ) ) {
 	/**
 	 * The url to the plugin directory.
 	 */
-	define( 'BBX_URL', plugin_dir_url( __FILE__ ) );
+	define( 'BENTOBOX_URL', plugin_dir_url( __FILE__ ) );
 }
 
-if ( ! defined( 'BBX_BASE_NAME' ) ) {
+if ( ! defined( 'BENTOBOX_BASE_NAME' ) ) {
 	/**
 	 * The url to the plugin directory.
 	 */
-	define( 'BBX_BASE_NAME', plugin_basename( __FILE__ ) );
+	define( 'BENTOBOX_BASE_NAME', plugin_basename( __FILE__ ) );
 }
 
-
-/**
- * Sets translated strings for a script.
- *
- * @return void
- */
-function bbx_set_script_translations() {
-	wp_set_script_translations( 'bbx-bento-box-script', 'bento-box' );
-}
-add_action( 'init', 'bbx_set_script_translations' );
-
-/**
- * Apply translation file as per WP language.
- */
-function bbx_text_domain_loader() {
-
-	// Get mo file as per current locale.
-	$mofile = BBX_PATH . 'languages/' . get_locale() . '.mo';
-
-	// If file does not exists, then apply default mo.
-	if ( ! file_exists( $mofile ) ) {
-		$mofile = BBX_PATH . 'languages/default.mo';
+// Sets translated strings for a script.
+if ( ! function_exists( 'bentobox_set_script_translations' ) ) :
+	/**
+	 * Sets translated strings for a script.
+	 *
+	 * @return void
+	 */
+	function bentobox_set_script_translations() {
+		wp_set_script_translations( 'bbx-bento-box-script', 'bento-box' );
 	}
+endif;
+add_action( 'init', 'bentobox_set_script_translations' );
 
-	load_textdomain( 'bento-box', $mofile );
-}
-add_action( 'plugins_loaded', 'bbx_text_domain_loader' );
+// Apply translation file as per WP language.
+if ( ! function_exists( 'bentobox_text_domain_loader' ) ) :
+	/**
+	 * Apply translation file as per WP language.
+	 */
+	function bentobox_text_domain_loader() {
 
-/**
- * Registers the block using the metadata loaded from the `block.json` file.
- * Behind the scenes, it registers also all assets so they can be enqueued
- * through the block editor in the corresponding context.
- *
- * @see https://developer.wordpress.org/reference/functions/register_block_type/
- */
-function bbx_bento_box_block_init() {
-	register_block_type( __DIR__ . '/build/bento-box' );
-	register_block_type( __DIR__ . '/build/bento-card' );
-}
-add_action( 'init', 'bbx_bento_box_block_init' );
+		// Get mo file as per current locale.
+		$mofile = BENTOBOX_PATH . 'languages/' . get_locale() . '.mo';
+
+		// If file does not exists, then apply default mo.
+		if ( ! file_exists( $mofile ) ) {
+			$mofile = BENTOBOX_PATH . 'languages/default.mo';
+		}
+
+		load_textdomain( 'bento-box', $mofile );
+	}
+endif;
+add_action( 'plugins_loaded', 'bentobox_text_domain_loader' );
+
+// Registers the block using the metadata loaded from the `block.json` file.
+if ( ! function_exists( 'bentobox_block_init' ) ) :
+	/**
+	 * Registers the block using the metadata loaded from the `block.json` file.
+	 * Behind the scenes, it registers also all assets so they can be enqueued
+	 * through the block editor in the corresponding context.
+	 *
+	 * @see https://developer.wordpress.org/reference/functions/register_block_type/
+	 */
+	function bentobox_block_init() {
+		register_block_type( __DIR__ . '/build/bento-box' );
+		register_block_type( __DIR__ . '/build/bento-card' );
+	}
+endif;
+add_action( 'init', 'bentobox_block_init' );
